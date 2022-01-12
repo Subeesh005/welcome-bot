febb8ca7955199dee4e3197bf02fb77bd87da2de
Subeesh005 <97532153+Subeesh005@users.noreply.github.com>
2022-01-12 08:03:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,16 +9,16 @@ def start(updater,context):
  updater.message.reply_text('''Hi iam welcome messanger bot 
 Add me to your group 
  
- Made with Love ❤️ by @lntechnical
+ Made with Love ❤️ by @subeesh005
 
   ''')
 def help(updater,context):
- updater.message.reply_text("Add me to your group ")
+ updater.message.reply_text("Add Me To Your Group ")
  
 
 def add_group(update: Update, context: CallbackContext):
     for member in update.message.new_chat_members:
-        update.message.reply_text(f'Hello {member.full_name} , Welcome to ln support Thank you for Joining  ')
+        update.message.reply_text(f'Hello {member.full_name} , Welcome Friend 💗 Thank You For Joining  ')
 
 add_group_handle = MessageHandler(Filters.status_update.new_chat_members, add_group)
 updater.dispatcher.add_handler(add_group_handle)
